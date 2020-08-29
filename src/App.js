5a6849cd6ec572f71710615f018f648dfa619460
BLAZE <blazemathew987@gmail.com>
2020-08-29 18:57:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form1 from './components/form';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><center>Login Form</center></h1>
+      <Form1 />
+    </div>
+  );
+}
+
+export default App;
