c97b1a80ac315e4208f506c6b92e7f4145494abe
BLAZE <blazemathew987@gmail.com>
2020-09-02 00:07:09 +0530

    added RC-field-form

--- src/components/form.js
+++ src/components/form.js
@@ -2,25 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 import 'antd/dist/antd.css';
 
-import { Form, Input, Button, Checkbox } from 'antd';
+import {  Input} from 'antd';
+import Form, { Field } from 'rc-field-form';
 
 
-const layout = {
-  labelCol: {
-    span: 7,
-  },
-  wrapperCol: {
-    span: 11,
-  },
-};
-const tailLayout = {
-  wrapperCol: {
-    offset: 6,
-    span: 10,
-  },
-};
+export default () => {
+  const [form] = Form.useForm();
 
-const Form1 = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -38,77 +26,56 @@ const Form1 = () => {
 }, []);
   
   
-  const onFinish = values => {
-    console.log('Success:', values);
-  };
 
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log(username);
-  };
 
   return (
     <div>
-      <Form
-      {...layout}
+      <form onSubmit={event => {
+      event.preventDefault();
+      event.stopPropagation();
+      form.validateFields().then(function (values) {
+        console.log(values);
+        
+        
+      }) // Do nothing about submit catch
+      .catch(function (e) {
+        return e;
+      });
+    }}>
+      <Form 
+        component={false} 
+        form={form}
+
       name="basic"
       initialValues={{
         remember: true,
       }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
+
       >
-        <Input
-         placeholder="User Name"
-         value={username} 
+        Username : 
+        <Field name="username">
+          <Input placeholder="Username" 
+          value={username} 
          onChange={event => {
            setUsername(event.target.value);
          }}/>
-    
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
+        </Field>
+        <br />
+        Password : 
+        <Field name="password">
         <Input.Password 
+        placeholder="Password"
         value={password}
         onChange={event => {
           setPassword(event.target.value);
-        }}
-        />
-      </Form.Item>
-
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit" onClick={e => onSubmitHandler(e)}>
+        }}/>
+        </Field>
+      </Form>
+      <button type="primary"  >
           Submit
-        </Button>
-      </Form.Item>
-    </Form>
+      </button>
+    </form>
+      
     <div>
         Post data with React Form: 
         <section>Username :{username}</section>
@@ -121,4 +88,3 @@ const Form1 = () => {
   );
 };
 
-export default Form1;
